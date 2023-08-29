fa344edd9df66de03ffb88792ab3e16e79a3f9c1
Pedro Carvalho <100386092+pedrocarvalhoit@users.noreply.github.com>
2023-08-29 22:42:35 +0100

    Sorting for Users list

--- BestshopWebParent/BestshopBackEnd/src/main/java/com/bestshop/admin/user/UserController.java
+++ BestshopWebParent/BestshopBackEnd/src/main/java/com/bestshop/admin/user/UserController.java
@@ -5,6 +5,7 @@ import com.bestshop.common.entity.Role;
 import com.bestshop.common.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.StringUtils;
@@ -23,12 +24,13 @@ public class UserController {
 
     @GetMapping("/users")
     public String listFirstPage(Model model){
-        return listByPage(1, model);
+        return listByPage(1, model, "firstName", "asc");
     }
 
     @GetMapping("/users/page/{pageNum}")
-    public String listByPage(@PathVariable(name = "pageNum") int pageNum, Model model){
-        Page<User> page = service.listByPage(pageNum);
+    public String listByPage(@PathVariable(name = "pageNum") int pageNum, Model model,@Param("sortField") String sortField,
+                             @Param("sortDir") String sortDir ){
+        Page<User> page = service.listByPage(pageNum, sortField, sortDir);
         List<User> listUsers = page.getContent();
 
         long startCount = (pageNum - 1) * UserService.USERS_PER_PAGE + 1;
@@ -43,6 +45,8 @@ public class UserController {
         model.addAttribute("endCount", endCount);
         model.addAttribute("totalItems", page.getTotalElements());
         model.addAttribute("listUsers", listUsers);
+        model.addAttribute("sortField", sortField);
+        model.addAttribute("sortDir", sortDir);
         return "users";
     }
 
