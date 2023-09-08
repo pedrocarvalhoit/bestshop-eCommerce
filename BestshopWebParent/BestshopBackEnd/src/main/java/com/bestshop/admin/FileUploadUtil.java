c0b981b64327b787358d75cd7508bafe61a9488c
Pedro Carvalho <100386092+pedrocarvalhoit@users.noreply.github.com>
2023-09-08 19:42:43 +0100

    HTML Personalized error pages

--- BestshopWebParent/BestshopBackEnd/src/main/java/com/bestshop/admin/FileUploadUtil.java
+++ BestshopWebParent/BestshopBackEnd/src/main/java/com/bestshop/admin/FileUploadUtil.java
@@ -1,5 +1,7 @@
 package com.bestshop.admin;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
@@ -12,6 +14,8 @@ import java.nio.file.StandardCopyOption;
 
 //Photo upload class
 public class FileUploadUtil {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(FileUploadUtil.class);
 
     public static void saveFile(String uploadDir, String fileName,
                                 MultipartFile multipartFile) throws IOException {
@@ -38,12 +42,14 @@ public class FileUploadUtil {
                     try {
                         Files.delete(file);
                     } catch (IOException ex) {
-                        System.out.println("Could not delete file: " + file);
+                        LOGGER.error("Could not delete file: " + file);
+                       // System.out.println("Could not delete file: " + file);
                     }
                 }
             });
         } catch (IOException ex) {
-            System.out.println("Could not list directory: " + dirPath);
+            LOGGER.error("Could not list directory: " + dirPath);
+ //           System.out.println("Could not list directory: " + dirPath);
         }
     }
 
