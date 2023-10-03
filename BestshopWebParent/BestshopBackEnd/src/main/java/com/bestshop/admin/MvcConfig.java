e99402b18a12571fd1cffc221906914d1187a879
Pedro Carvalho <100386092+pedrocarvalhoit@users.noreply.github.com>
2023-10-03 20:21:28 +0100

    Code Refactor #5

--- BestshopWebParent/BestshopBackEnd/src/main/java/com/bestshop/admin/MvcConfig.java
+++ BestshopWebParent/BestshopBackEnd/src/main/java/com/bestshop/admin/MvcConfig.java
@@ -13,33 +13,21 @@ public class MvcConfig implements WebMvcConfigurer {
     //Expose a directory on the file system - to be accessible by the clients
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        String dirName = "user-photos";//This defines the name of the directory containing user photos.
-        Path userPhotosDir = Paths.get("user-photos");//object representing the directory where user photos are stored.
+        exposeDiretory("user-photos", registry);
+        exposeDiretory("../category-images", registry);
+        exposeDiretory("../brand-logos", registry);
 
-        String userPhotosPath = userPhotosDir.toFile().getAbsolutePath();
 
-        registry.addResourceHandler("/" + dirName + "/**")// any request for a URL that starts with /user-photos/ will be handled by this resource handler.
-                .addResourceLocations("file:/" + userPhotosPath + "/");
+    }
 
-        // Categories
+    private void exposeDiretory(String pathPattern, ResourceHandlerRegistry registry){
+        Path path = Paths.get(pathPattern);
+        String absolutePath = path.toFile().getAbsolutePath();
 
-        String categoryImagesDirName = "../category-images";
-        Path categoryImagesDir = Paths.get(categoryImagesDirName);
+        String logicalPath = pathPattern.replace("../", "") + "/**";
 
-        String categoryImagesPath = categoryImagesDir.toFile().getAbsolutePath();
-
-        registry.addResourceHandler("/category-images/**")
-                .addResourceLocations("file:/" + categoryImagesPath + "/");
-
-        // Brands
-
-        String brandLogosDirName = "../brand-logos";
-        Path brandLogosDir = Paths.get(brandLogosDirName);
-
-        String brandLogosPath = brandLogosDir.toFile().getAbsolutePath();
-
-        registry.addResourceHandler("/brand-logos/**")
-                .addResourceLocations("file:/" + brandLogosPath + "/");
+        registry.addResourceHandler(logicalPath)
+                .addResourceLocations("file:/" + absolutePath + "/");
 
     }
 
