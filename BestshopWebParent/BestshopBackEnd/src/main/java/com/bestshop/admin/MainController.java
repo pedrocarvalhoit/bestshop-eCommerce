32e63c688b311f4ef5cf2815c6691bde9d8b4f83
Pedro Carvalho <100386092+pedrocarvalhoit@users.noreply.github.com>
2023-11-13 21:18:03 +0000

    Code Refactor #6

--- BestshopWebParent/BestshopBackEnd/src/main/java/com/bestshop/admin/MainController.java
+++ BestshopWebParent/BestshopBackEnd/src/main/java/com/bestshop/admin/MainController.java
@@ -1,5 +1,8 @@
 package com.bestshop.admin;
 
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -8,18 +11,19 @@ import org.springframework.web.bind.annotation.PostMapping;
 public class MainController {
 
     @GetMapping("/home")
-    public String viewHomePage(){
+    public String viewHomePage() {
         return "index";
     }
 
     @GetMapping("/login")
-    public String viewLoginPage(){
-        return "/login";
+    public String viewLoginPage() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication == null || authentication instanceof AnonymousAuthenticationToken){
+            return "login";
+        }
+
+        return "redirect:/";
     }
 
-//    @GetMapping("/bs/logout")
-//    public String performLogout() {
-//        // .. perform logout
-//        return "redirect:/login&logout";
-//    }
 }
+
