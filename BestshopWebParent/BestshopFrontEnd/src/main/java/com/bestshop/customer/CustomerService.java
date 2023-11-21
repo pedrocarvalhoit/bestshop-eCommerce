2e4c3659f750308e5a00a9cad68eaf9b7afd8d8c
Pedro Carvalho <100386092+pedrocarvalhoit@users.noreply.github.com>
2023-11-21 15:31:15 +0000

    Code Verify Customer Account

--- BestshopWebParent/BestshopFrontEnd/src/main/java/com/bestshop/customer/CustomerService.java
+++ BestshopWebParent/BestshopFrontEnd/src/main/java/com/bestshop/customer/CustomerService.java
@@ -3,6 +3,7 @@ package com.bestshop.customer;
 import com.bestshop.common.entity.Country;
 import com.bestshop.common.entity.Customer;
 import com.bestshop.setting.CountryRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -13,6 +14,7 @@ import java.util.Date;
 import java.util.List;
 
 @Service
+@Transactional
 public class CustomerService {
 
     @Autowired
@@ -52,5 +54,16 @@ public class CustomerService {
     private void passwordEncoder(Customer customer) {
         String encodedPassword = passwordEncoder.encode(customer.getPassword());
         customer.setPassword(encodedPassword);
+    }
+
+    public boolean verify(String verificationCode) {
+        Customer customer = customerRepo.findByVerificationCode(verificationCode);
+
+        if (customer == null || customer.isEnabled()) {
+            return false;
+        } else {
+            customerRepo.enable(customer.getId());
+            return true;
+        }
     }
 }
