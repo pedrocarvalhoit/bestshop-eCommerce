8edb851ceb434988faa6a83dc0fde6f0b539ee97
Pedro Carvalho <100386092+pedrocarvalhoit@users.noreply.github.com>
2023-12-04 20:50:21 +0000

    Code Shipping Rates Management Module

---
+++ BestshopWebParent/BestshopBackEnd/src/main/java/com/bestshop/admin/shippingrate/ShippingRateNotFoundException.java
@@ -0,0 +1,9 @@
+package com.bestshop.admin.shippingrate;
+
+public class ShippingRateNotFoundException extends Exception {
+
+    public ShippingRateNotFoundException(String message) {
+        super(message);
+    }
+
+}
