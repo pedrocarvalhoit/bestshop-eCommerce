38b87fbd39800f2da1df1a7ac886775b8bc5f0eb
Pedro Carvalho <100386092+pedrocarvalhoit@users.noreply.github.com>
2023-12-05 11:00:01 +0000

    Code Repository Layer for Order Module

---
+++ BestshopCommon/src/main/java/com/bestshop/common/entity/PaymentMethod.java
@@ -0,0 +1,5 @@
+package com.bestshop.common.entity;
+
+public enum PaymentMethod {
+    COD, CREDIT_CARD
+}
---
+++ BestshopWebParent/BestshopBackEnd/src/main/java/com/bestshop/admin/order/OrderRepository.java
@@ -0,0 +1,8 @@
+package com.bestshop.admin.order;
+
+import com.bestshop.common.entity.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderRepository extends JpaRepository<Order, Integer> {
+
+}
