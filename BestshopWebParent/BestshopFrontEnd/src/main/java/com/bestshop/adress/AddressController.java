b63ccb8b9b9120808ce27c780a5b6223a6a12436
Pedro Carvalho <100386092+pedrocarvalhoit@users.noreply.github.com>
2023-12-06 17:27:16 +0000

    Code Shipping Address from CheckoutPage

--- BestshopWebParent/BestshopFrontEnd/src/main/java/com/bestshop/adress/AddressController.java
+++ BestshopWebParent/BestshopFrontEnd/src/main/java/com/bestshop/adress/AddressController.java
@@ -65,9 +65,17 @@ public class AddressController {
         address.setCustomer(customer);
         addressService.save(address);
 
+        String redirectOption = request.getParameter("redirect");
+        String redirectURL = "redirect:/address_book";
+
+        if ("checkout".equals(redirectOption)) {
+            redirectURL += "?redirect=checkout";
+        }
+
+
         ra.addFlashAttribute("message", "The address has been saved successfully.");
 
-        return "redirect:/address_book";
+        return redirectURL;
     }
 
     @GetMapping("/address_book/edit/{id}")
@@ -107,6 +115,8 @@ public class AddressController {
 
         if ("cart".equals(redirectOption)) {
             redirectURL = "redirect:/cart";
+        }else if ("checkout".equals(redirectOption)) {
+            redirectURL = "redirect:/checkout";
         }
 
         return redirectURL;
