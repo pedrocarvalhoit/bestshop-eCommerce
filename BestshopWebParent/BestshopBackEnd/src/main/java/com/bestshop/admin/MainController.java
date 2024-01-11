7b3fb436d280e3b58074823e779f2cdb3b9de1b4
Pedro Carvalho <100386092+pedrocarvalhoit@users.noreply.github.com>
2024-01-11 16:45:08 +0000

    Code dashboard part 1

--- BestshopWebParent/BestshopBackEnd/src/main/java/com/bestshop/admin/MainController.java
+++ BestshopWebParent/BestshopBackEnd/src/main/java/com/bestshop/admin/MainController.java
@@ -1,17 +1,26 @@
 package com.bestshop.admin;
 
+import com.bestshop.admin.dashboard.DashboardInfo;
+import com.bestshop.admin.dashboard.DashboardService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MainController {
 
-    @GetMapping("/home")
-    public String viewHomePage() {
+    @Autowired
+    private DashboardService dashboardService;
+
+    @GetMapping("")
+    public String viewHomePage(Model model) {
+        DashboardInfo summary = dashboardService.loadSummary();
+        model.addAttribute("summary", summary);
         return "index";
     }
 
